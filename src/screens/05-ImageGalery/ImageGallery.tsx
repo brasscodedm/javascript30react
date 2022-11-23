7fc223eaab0d7ab545830549e12c14087f386ea3
Dominik <d>
2022-11-23 17:23:13 +0100

    remove dirty code

--- src/screens/05-ImageGalery/ImageGallery.tsx
+++ src/screens/05-ImageGalery/ImageGallery.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { TransitionEvent, useState } from 'react';
 import styles from './ImageGallery.module.css';
 import cx from 'classnames';
 
@@ -6,18 +6,43 @@ interface Panel {
   top: string;
   mid: string;
   bottom: string;
+  url: string;
 }
 
 const panels: Panel[] = [
-  { top: 'Hey', mid: `Let's`, bottom: 'Dance' },
-  { top: 'Give', mid: 'Take', bottom: 'Receive' },
-  { top: 'Experience', mid: 'It', bottom: 'Today' },
-  { top: 'Give', mid: 'All', bottom: 'You can' },
-  { top: 'Life', mid: 'In', bottom: 'Motion' },
+  {
+    top: 'Hey',
+    mid: `Let's`,
+    bottom: 'Dance',
+    url: 'https://source.unsplash.com/gYl-UtwNg_I/1500x1500',
+  },
+  {
+    top: 'Give',
+    mid: 'Take',
+    bottom: 'Receive',
+    url: 'https://source.unsplash.com/rFKUFzjPYiQ/1500x1500',
+  },
+  {
+    top: 'Experience',
+    mid: 'It',
+    bottom: 'Today',
+    url: 'https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=1500&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d',
+  },
+  {
+    top: 'Give',
+    mid: 'All',
+    bottom: 'You can',
+    url: 'https://source.unsplash.com/ITjiVXcwVng/1500x1500',
+  },
+  {
+    top: 'Life',
+    mid: 'In',
+    bottom: 'Motion',
+    url: 'https://source.unsplash.com/3MNzGlQM7qs/1500x1500',
+  },
 ];
 
 export const ImageGallery = () => {
-  const [transitionedPanels, setTransitionedPanels] = useState<string[]>([]);
   const [activePanels, setActivePanels] = useState<string[]>([]);
 
   const handleClick = (panel: string) => {
@@ -28,14 +53,6 @@ export const ImageGallery = () => {
     }
   };
 
-  const handleTransitionEnd = (panel: string) => {
-    if (transitionedPanels.includes(panel)) {
-      setTransitionedPanels(prevState => prevState.filter(p => p !== panel));
-    } else {
-      setTransitionedPanels(prevState => [...prevState, panel]);
-    }
-  };
-
   return (
     <div className={cx(styles.panels)}>
       {panels.map((panel, key) => {
@@ -43,14 +60,13 @@ export const ImageGallery = () => {
         return (
           <div
             onClick={() => handleClick(panelName)}
-            onTransitionEnd={() => handleTransitionEnd(panelName)}
             key={key}
             className={cx(
               styles.panel,
               styles[panelName],
-              activePanels.includes(panelName) && styles.open,
-              transitionedPanels.includes(panelName) && styles.openActive
+              activePanels.includes(panelName) && styles.open
             )}
+            style={{ backgroundImage: `url(${panel.url})` }}
           >
             <p>{panel.top}</p>
             <p>{panel.mid}</p>
